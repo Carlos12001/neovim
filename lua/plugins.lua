1f8ca83c4c1a94eec12553f2ece3bf46ede41d41
Carlos12001 <carlos.andres12001@gmail.com>
2024-09-13 18:45:26 -0600

    fix: commet broken pluggins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,11 +49,11 @@ return {
   },
 
   -- Telescope
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function() require("configs.telescope")() end,
-  },
+  -- {
+  --   'nvim-telescope/telescope.nvim',
+  --   dependencies = { 'nvim-lua/plenary.nvim' },
+  --   config = function() require("configs.telescope")() end,
+  -- },
 
   -- Mason
   -- {
